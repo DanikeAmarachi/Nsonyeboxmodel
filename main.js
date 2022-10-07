50f0644b5fae84a49ebd6be1334af7641bd6f8d5
DanikeAmarachi <danikeamarachi@gmail.com>
2022-10-07 18:36:01 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,80 @@
+document.getElementById("Faith").innerHTML = "Today is the last class for nsonye";
+
+let a, b, c, d, e;
+a = 7
+b = 5
+c = 9
+d = 13
+e = 15
+f = a + b;
+console.log(f);
+
+function precious() {
+document.getElementById("timi").innerHTML = "This is Timi's record"
+document.getElementById("kengi").innerHTML = "This is kengi's record"
+}
+
+let carName = "Camry"; 
+let carName1 =  "Volvo";
+let carName2 =  "Pathfinder"; 
+let carName3 = "Toyota";
+let carName4 = "Benz";
+
+document.getElementById('car1').innerHTML = carName;
+document.getElementById('car2').innerHTML = carName1;
+document.getElementById('car3').innerHTML = carName2;
+document.getElementById('car4').innerHTML = carName3;
+document.getElementById('car5').innerHTML = carName4;
+
+document.getElementById("Faith").innerHTML = "Today's the last class for the Second Cohort";
+
+ let a, b, c, d, e;
+ a = 5
+ b = 7
+ c = 9
+ d = 13
+ e = 15 
+ f = a + b;
+ console.log(f);
+
+ function precious() {
+document.getElementById("timi").innerHTML = "This is Timi's record"
+document.getElementById("kengi").innerHTML = "This is Kengi's record"
+
+}
+
+// Data types
+
+//"Data" 'Data' // A string 
+//1272738 //Numbers or Integers
+//28.04900 //Float
+
+g = 20 + "5"
+console.log(g)
+
+
+// = This is an Assignment Operator
+// == This is a comparison operator
+
+statement = "This program is truly sponsored by" + " " + "Nnsonye" 
+console.log(statement)
+
+listOfnames = "Faith " + "Timi" + " Danike"
+console.log(listOfnames)
+
+/* let x, y;
+x = 5 + 6;
+y = x * 10;
+
+var p, u;
+p = 5 + 6;
+u = x * 10; */
+
+// Let's do a simple intrest using const
+
+const principal = 10;
+const rate = 4;
+const time = 21;
+let simpleIntrest = (principal * rate * time) / 100
+document.getElementById("sp").innerHTML = "The Simple intrest of the expression is " + simpleIntrest;
+console.log(simpleIntrest)
